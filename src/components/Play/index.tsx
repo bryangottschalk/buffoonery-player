fc2107a5dccddda95f19c2289d25b5ce12610d6c
bryangottschalk <bryanlgottschalk@gmail.com>
2020-12-06 17:57:45 -0600

    connection to websocket framework

--- src/components/Play/index.tsx
+++ src/components/Play/index.tsx
@@ -1,7 +1,12 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
+import {
+  fetchConnectToRoom,
+  setMyConnectionUrl
+} from '../../store/slices/application';
+import { useDispatch } from 'react-redux';
 
 interface Props {}
 const useStyles = makeStyles((theme) => ({
@@ -13,13 +18,18 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const handleClick = () => {
-  alert(`Sorry, this feature isn't ready yet.`);
-};
-
 export default function Play({}: Props): ReactElement {
+  const [roomcode, setRoomcode] = useState('');
+  const [name, setName] = useState('');
+  const dispatch = useDispatch();
   const classes = useStyles();
-
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log('Roomcode:', roomcode, 'Name:', name);
+    const connectionUrl = `wss://da6wisihu2.execute-api.us-east-1.amazonaws.com/dev?roomcode=${roomcode}`;
+    dispatch(setMyConnectionUrl(connectionUrl));
+    dispatch(fetchConnectToRoom(connectionUrl));
+  };
   return (
     <div style={{ margin: '0px 16px' }}>
       <div style={{ marginBottom: 20 }}>
@@ -29,10 +39,25 @@ export default function Play({}: Props): ReactElement {
         </a>
         .
       </div>
-      <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="Roomcode" variant="outlined" />
-        <TextField id="outlined-basic" label="Name" variant="outlined" />
-        <Button onClick={handleClick} variant="contained" color="primary">
+      <form
+        onSubmit={handleSubmit}
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          onInput={(e: any) => setRoomcode(e.target.value)}
+          id="outlined-basic"
+          label="Roomcode"
+          variant="outlined"
+        />
+        <TextField
+          onInput={(e: any) => setName(e.target.value)}
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">
           Play
         </Button>
       </form>
--- src/store/slices/application.tsx
+++ src/store/slices/application.tsx
@@ -2,31 +2,91 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 interface ApplicationState {
-  myConnectionId: string;
+  websocket: any;
+  myConnectionUrl: string;
   navigationTab: string;
+  isConnected: boolean;
+  loading: boolean;
+  hasErrors: boolean;
+  errorMsg: string;
 }
 
 const initialState: ApplicationState = {
-  myConnectionId: '',
-  navigationTab: ''
+  websocket: null,
+  myConnectionUrl: '',
+  navigationTab: '',
+  isConnected: false,
+  loading: false,
+  hasErrors: false,
+  errorMsg: ''
 };
 
 const applicationSlice = createSlice({
   name: 'application',
   initialState,
   reducers: {
-    setMyConnectionId: (state, { payload }) => {
-      state.myConnectionId = payload;
+    setMyConnectionUrl: (state, { payload }) => {
+      state.myConnectionUrl = payload;
     },
     setNavigationTab: (state, { payload }) => {
       state.navigationTab = payload;
+    },
+    connectToRoom: (state) => {
+      state.loading = true;
+    },
+    connectToRoomFailure: (state, { payload }) => {
+      state.loading = false;
+      state.isConnected = false;
+      state.hasErrors = true;
+      state.errorMsg = payload;
+    },
+    connectToRoomSuccess: (state) => {
+      state.isConnected = true;
+      state.loading = false;
+      state.hasErrors = false;
     }
   }
 });
 
-export const { setMyConnectionId, setNavigationTab } = applicationSlice.actions;
+export const {
+  setMyConnectionUrl,
+  setNavigationTab,
+  connectToRoom,
+  connectToRoomSuccess,
+  connectToRoomFailure
+} = applicationSlice.actions;
 
 export const getApplicationState = (state: { application: any }) =>
   state.application;
+
+export const fetchConnectToRoom = createAsyncThunk(
+  'application/fetchConnectToRoom',
+  async (connectionUrl: string, thunkAPI: any) => {
+    try {
+      thunkAPI.dispatch(connectToRoom());
+      const ws = new WebSocket(connectionUrl);
+      if (ws) {
+        ws.onopen = async () => {
+          const connectMsg = {
+            action: 'sendmessage',
+            data: {
+              msg: `CONNECTION OPENED IN ROOM: ${connectionUrl}`,
+              roomcode: ''
+            }
+          };
+          ws.send(JSON.stringify(connectMsg));
+        };
+
+        thunkAPI.dispatch(connectToRoomSuccess());
+      } else {
+        thunkAPI.dispatch(
+          connectToRoomFailure(new Error('error connecting to room'))
+        );
+      }
+    } catch (err) {
+      thunkAPI.dispatch(connectToRoomFailure(err));
+    }
+  }
+);
 
 export default applicationSlice.reducer;
