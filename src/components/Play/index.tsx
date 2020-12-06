b138344f28662305e7ef3bd8e8bf3cb1000959b0
bryangottschalk <bryanlgottschalk@gmail.com>
2020-12-06 16:01:04 -0600

    fix link

--- src/components/Play/index.tsx
+++ src/components/Play/index.tsx
@@ -24,7 +24,10 @@ export default function Play({}: Props): ReactElement {
     <div style={{ margin: '0px 16px' }}>
       <div style={{ marginBottom: 20 }}>
         Enter your roomcode from the host site. To start a lobby go to{' '}
-        <a href="host.buffoonery.io">host.buffoonery.io</a>.
+        <a href="https://host.buffoonery.io" target="_blank">
+          host.buffoonery.io
+        </a>
+        .
       </div>
       <form className={classes.root} noValidate autoComplete="off">
         <TextField id="outlined-basic" label="Roomcode" variant="outlined" />
