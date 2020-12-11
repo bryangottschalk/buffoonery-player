b00cc0298fd64d2e88a7d7194c78a595c7856e6c
bryangottschalk <bryanlgottschalk@gmail.com>
2020-12-11 10:12:18 -0600

    disabled form state

--- src/components/Play/index.tsx
+++ src/components/Play/index.tsx
@@ -21,6 +21,13 @@ const useStyles = makeStyles((theme) => ({
 export default function Play({}: Props): ReactElement {
   const [roomcode, setRoomcode] = useState('');
   const [name, setName] = useState('');
+  const [isPlayBtnDisabled, setIsPlayBtnDisabled] = useState(true)
+  if (roomcode.length === 4 && name.length > 0 && isPlayBtnDisabled) {
+    setIsPlayBtnDisabled(false)
+  }
+  if ((roomcode.length < 4 || name.length === 0) && !isPlayBtnDisabled) {
+    setIsPlayBtnDisabled(true)
+  } 
   const dispatch = useDispatch();
   const classes = useStyles();
   const handleSubmit = (event: any) => {
@@ -33,7 +40,7 @@ export default function Play({}: Props): ReactElement {
   return (
     <div style={{ margin: '0px 16px' }}>
       <div style={{ marginBottom: 20 }}>
-        Enter your roomcode from the host site. To start a lobby go to{' '}
+        Enter your roomcode from the host site and a username for the game. To start a new room go to{' '}
         <a href="https://host.buffoonery.io" target="_blank">
           host.buffoonery.io
         </a>
@@ -46,18 +53,22 @@ export default function Play({}: Props): ReactElement {
         autoComplete="off"
       >
         <TextField
+          required
           onInput={(e: any) => setRoomcode(e.target.value)}
           id="outlined-basic"
           label="Roomcode"
           variant="outlined"
+          inputProps={{ maxLength: 4 }}
         />
         <TextField
+          required
           onInput={(e: any) => setName(e.target.value)}
           id="outlined-basic"
           label="Name"
           variant="outlined"
+          inputProps={{ maxLength: 30 }}
         />
-        <Button type="submit" variant="contained" color="primary">
+        <Button disabled={isPlayBtnDisabled} type="submit" variant="contained" color="primary">
           Play
         </Button>
       </form>
